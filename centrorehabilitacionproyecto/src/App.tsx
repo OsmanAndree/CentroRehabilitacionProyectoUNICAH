b6719e1e1b75adc50c8c51a579abc4a016429f84
Lenard98 <lenardsalinas5@gmail.com>
2025-03-09 18:08:09 -0600

    Realizacion de Login Y Arreglos de App

--- centrorehabilitacionproyecto/src/App.tsx
+++ centrorehabilitacionproyecto/src/App.tsx
@@ -1,27 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import NavBar from './components/share/NavBar';
 import Home from './components/Home';
 import PacientesTable from "./components/Pacientes";
+import TerapeutasTable from './components/Terapeuta';
 import CitasTable from "./components/Citas";
+import Login from './components/Login';  
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import TerapeutasTable from './components/Terapeuta';
 
 function App() {
+  const isAuthenticated = localStorage.getItem("user"); 
+
   return (
     <Router>
-      <NavBar />
+      <ToastContainer />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pacientes" element={<PacientesTable />} />
-          <Route path="/terapeutas" element={<TerapeutasTable />} /> 
-          <Route path="/citas" element={<CitasTable />} />
+        
+          <Route path="/" element={<Login />} />
+
+          {isAuthenticated ? (
+            <>
+              <Route path="/home" element={<><NavBar /><Home /></>} />
+              <Route path="/pacientes" element={<><NavBar /><PacientesTable /></>} />
+              <Route path="/terapeutas" element={<><NavBar /><TerapeutasTable /></>} />
+              <Route path="/citas" element={<><NavBar /><CitasTable /></>} />
+            </>
+          ) : (
+            <Navigate to="/" />
+          )}
         </Routes>
       </main>
-      <ToastContainer />
     </Router>
   );
 }
--- centrorehabilitacionproyecto/src/components/Login.tsx
+++ centrorehabilitacionproyecto/src/components/Login.tsx
@@ -1,45 +1,75 @@
 import React, { useState, FormEvent } from 'react';
 import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; 
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
   
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [usuario, setusuario] = useState<string>('');
+  const [contraseña, setcontraseña] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   
   const handleLogin = (e: FormEvent) => {
     e.preventDefault();
-    navigate('/home');
+    navigate('/Home');
+  };
+
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState); 
   };
 
   return (
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh', padding: '0 15px' }}>
+    <Container fluid className="vh-90 d-flex justify-content-center align-items-center">
       <Row className="w-100">
-        <Col md={6} className="mx-auto">
-          <div className="card p-5 shadow-lg" style={{ minWidth: '300px', maxWidth: '400px' }}>
-            <h2 className="text-center mb-4">Acceso Rápido</h2>
+        <Col md={6} className="mx-auto d-flex justify-content-center">
+          <div className="card p-4 shadow-lg bg-white" style={{ minWidth: '400px', maxWidth: '400px', borderRadius: '10px' }}>
+            <div className="text-center mt-3">
+              <img src="/logo.svg" alt="Logo" style={{ width: '150px' }} />
+            </div>
+            <h2 className="text-center mb-4 text-dark" style={{ margin: '0 0 0px' }}>Iniciar Sesion</h2>
             <Form onSubmit={handleLogin}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
+              <Form.Group className="mb-3" controlId="formBasicUsser">
+                <Form.Label>Usuario</Form.Label>
                 <Form.Control 
-                  type="email" 
-                  placeholder="Introduce tu email" 
-                  value={email} 
-                  onChange={(e) => setEmail(e.target.value)} 
+                  type="text" 
+                  placeholder="Introduce tu Usuario" 
+                  value={usuario} 
+                  onChange={(e) => setusuario(e.target.value)} 
                   required
+                  style={{ borderRadius: '0.375rem' }} 
                 />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Contraseña</Form.Label>
-                <Form.Control 
-                  type="password" 
-                  placeholder="Introduce tu contraseña" 
-                  value={password} 
-                  onChange={(e) => setPassword(e.target.value)} 
-                  required
-                />
+                <div className="d-flex align-items-center">
+                  <Form.Control 
+                    type={showPassword ? 'text' : 'password'} 
+                    placeholder="Introduce tu contraseña" 
+                    value={contraseña} 
+                    onChange={(e) => setcontraseña(e.target.value)} 
+                    required
+                    style={{
+                      borderRadius: '0.375rem', 
+                      paddingLeft: '10px', 
+                      paddingRight: '35px', 
+                    }}
+                  />
+                  <Button 
+                    variant="link" 
+                    onClick={togglePasswordVisibility} 
+                    className="text-decoration-none" 
+                    style={{
+                      marginLeft: '-30px', 
+                      fontSize: '1.2rem',
+                      padding: 0,
+                    }}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />} 
+                  </Button>
+                </div>
               </Form.Group>
 
               <Button variant="primary" type="submit" className="w-100">
