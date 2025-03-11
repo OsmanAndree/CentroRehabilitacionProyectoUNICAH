422e41bc974d1053ac13f568d6269d264f05098e
osman barahona <osma_barahonas01@unicah.edu>
2025-03-10 19:03:13 -0600

    Cambios de Diseño General

--- centrorehabilitacionproyecto/src/components/share/NavBar.tsx
+++ centrorehabilitacionproyecto/src/components/share/NavBar.tsx
@@ -1,61 +1,73 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar, Nav, Container, Button, Form, NavDropdown, Offcanvas, InputGroup } from "react-bootstrap";
-import { FaSearch, FaUser, FaBox, FaWarehouse, FaClipboardList, FaHome, FaCalendarAlt, FaNotesMedical, FaUsers, FaUsersCog, FaUserFriends, FaBriefcaseMedical, FaClinicMedical, FaBuyNLarge, FaBuysellads, FaMoneyBill, FaSignOutAlt } from "react-icons/fa";
+import { Navbar, Nav, Container, Button, Form, NavDropdown, Offcanvas } from "react-bootstrap";
+import { FaUser, FaBox, FaWarehouse, FaClipboardList, FaHome, FaCalendarAlt, FaNotesMedical, FaUsers, FaUserFriends, FaClinicMedical, FaMoneyBill, FaSignOutAlt } from "react-icons/fa";
 
 function NavBar() {
   const navigate = useNavigate();
+  const [showOffcanvas, setShowOffcanvas] = useState<boolean>(false);
+
+  const handleNavClick = (path: string) => {
+    navigate(path);
+    setShowOffcanvas(false);
+  };
 
   return (
     <Navbar expand="lg" fixed="top" style={{ backgroundColor: "#287549" }} className="shadow">
       <Container fluid>
-
-        <Navbar.Brand onClick={() => navigate("/home")} className="brand-hover fw-bold text-white">
+        <Navbar.Brand onClick={() => handleNavClick("/home")} className="brand-hover fw-bold text-white">
           <img src="/logo.png" alt="Logo" width="50" height="50" className="d-inline-block align-center me-2" />
           Centro de Rehabilitación
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="offcanvasNavbar" className="border-0 shadow-none text-white" />
-        <Navbar.Offcanvas id="offcanvasNavbar" placement="end" className="offcanvas-custom" >
+        <Navbar.Toggle aria-controls="offcanvasNavbar" className="border-0 shadow-none text-white" onClick={() => setShowOffcanvas(true)} />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          placement="end"
+          className="offcanvas-custom"
+          show={showOffcanvas}
+          onHide={() => setShowOffcanvas(false)}
+        >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title className="fw-bold text-white">Centro de Rehabilitación</Offcanvas.Title>
+            <Offcanvas.Title className="fw-bold text-white">Menú Desplegable</Offcanvas.Title>
           </Offcanvas.Header>
 
           <Offcanvas.Body>
             <Nav className="justify-content-center flex-grow-1 pe-3">
-              <Nav.Link onClick={() => navigate("/home")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/home")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaHome className="me-2" /> Inicio
               </Nav.Link>
-              <Nav.Link onClick={() => navigate("/citas")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/citas")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaCalendarAlt className="me-2" /> Citas
               </Nav.Link>
-              <Nav.Link onClick={() => navigate("/diagnosticos")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/diagnosticos")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaNotesMedical className="me-2" /> Diagnósticos
               </Nav.Link>
-              <Nav.Link onClick={() => navigate("/pacientes")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/pacientes")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaUsers className="me-2" /> Pacientes
               </Nav.Link>
-              <Nav.Link onClick={() => navigate("/encargados")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/encargados")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaUserFriends className="me-2" /> Encargados
               </Nav.Link>
-              <Nav.Link onClick={() => navigate("/terapeutas")} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
+              <Nav.Link onClick={() => handleNavClick("/terapeutas")} className="fw-bold text-white nav-hover mb-2" style={{ fontSize: '18px' }}>
                 <FaClinicMedical className="me-2" /> Terapeutas
               </Nav.Link>
 
-              <NavDropdown title={<span style={{ color: 'white' }}>Accesos Rápidos</span>} className="fw-bold text-white nav-hover" style={{ fontSize: '18px' }}>
-              <NavDropdown.Item onClick={() => navigate("/compras")}>
+              <NavDropdown title={<span style={{ color: 'white' }}>Accesos Rápidos</span>} className="fw-bold text-white nav-hover mb-3" style={{ fontSize: '18px' }}>
+                <NavDropdown.Item onClick={() => handleNavClick("/compras")}>
                   <FaMoneyBill className="me-2 text-success" /> Compras
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate("/prestamos")}>
+                <NavDropdown.Item onClick={() => handleNavClick("/prestamos")}>
                   <FaClipboardList className="me-2 text-success" /> Préstamos
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate("/productos")}>
+                <NavDropdown.Item onClick={() => handleNavClick("/productos")}>
                   <FaBox className="me-2 text-success" /> Productos
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate("/bodegas")}>
+                <NavDropdown.Item onClick={() => handleNavClick("/bodegas")}>
                   <FaWarehouse className="me-2 text-success" /> Bodegas
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item onClick={() => navigate("/usuarios")}>
+                <NavDropdown.Item onClick={() => handleNavClick("/usuarios")}>
                   <FaUser className="me-2 text-danger" /> Usuarios
                 </NavDropdown.Item>
               </NavDropdown>
@@ -65,7 +77,7 @@ function NavBar() {
               <Button variant="danger" onClick={() => {
                 window.location.href = "/";
               }}
-              className="d-fñlex align-items-center gap-2">
+              className="d-flex align-items-center gap-2">
                 <FaSignOutAlt /> Cerrar Sesión
               </Button>
             </Form>
