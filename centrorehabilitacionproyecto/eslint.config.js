60cb77e7ada4f64019957be28ba8c8278123d73a
Osmancito1 <osma_barahonas@unicah.edu>
2025-08-10 16:30:19 -0600

    chore: cambios en GlobalToast

--- centrorehabilitacionproyecto/eslint.config.js
+++ centrorehabilitacionproyecto/eslint.config.js
@@ -11,5 +11,6 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"], // <-- Agrega esta línea
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
 ]);
